be58a8a704922401fe0bc27784151d70bbe11735
Jordan Kicklighter <jordan@shiftagent.org>
2014-06-18 10:04:48 -0400

    Make the install script work regardless of where shift_landing is installed

--- install.sh
+++ install.sh
@@ -2,4 +2,16 @@
 #
 # Symlinks styles into project styles
 
-ln -s app/styles/base ../../styles/base
+x=`pwd`
+#now lets cut off /components/shift_basecss
+
+x="${x%/*}"
+x="${x%/*}"
+#x now looks like ~/path/to/shift_project/app
+
+echo "Linking..."
+echo "       ${x}/components/shift_basecss/styles/base"
+echo "to:    ${x}/styles/base"
+ln -s ${x}/components/shift_basecss/styles/base ${x}/styles/base
+
+echo "shift_basecss: All linked up!"
